16d724200fc657147c9747ac3b9b0f3e01c8db41
T1WiLLi <williambeaudun@gmail.com>
2024-05-25 21:23:19 -0400

    Fixed button icon setVisible

--- frontend/desktop/cms/src/main/java/com/cms/controller/MenuController.java
+++ frontend/desktop/cms/src/main/java/com/cms/controller/MenuController.java
@@ -1,35 +1,84 @@
 package com.cms.controller;
 
-import javafx.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
-public class MenuController{
-
-    @FXML
-    private Button button;
+public class MenuController {
 
     @FXML
-    private ImageView icon1;
+    Pane pane;
+
+    private Button[] buttons;
+
+    private ImageView[] icons;
 
     @FXML
     private void initialize() {
-        icon1.setVisible(true);
+        ObservableList<Node> children = pane.getChildren();
+        List<Button> buttonList = new ArrayList<>();
+        List<ImageView> iconList = new ArrayList<>();
+
+        for (Node node : children) {
+            if (node instanceof Button) {
+                buttonList.add((Button) node);
+            } else if (node instanceof ImageView) {
+                iconList.add((ImageView) node);
+            }
+        }
+
+        buttons = buttonList.toArray(new Button[0]);
+        icons = iconList.toArray(new ImageView[0]);
+
+        boundEventListenerToButtonElement();
     }
 
-    @FXML
-    private void handleMouseEnter() {
-      
-        icon1.setOpacity(0);
+    private void boundEventListenerToButtonElement() {
+        for (Button button : buttons) {
+            button.setOnMouseEntered(e -> {
+                ImageView associatedIcon = findAssociatedIcon(button);
+                if (associatedIcon != null) {
+                    associatedIcon.setVisible(false);
+                }
+            });
+
+            button.setOnMouseExited(event -> {
+                ImageView associatedIcon = findAssociatedIcon(button);
+                if (associatedIcon != null) {
+                    associatedIcon.setVisible(true);
+                }
+            });
+        }
     }
 
-    @FXML
-    private void handleMouseExit() {
-        icon1.setVisible(true);
-    }
+    private ImageView findAssociatedIcon(Button button) {
+        ImageView closestIcon = null;
+        double closestDistance = Double.MAX_VALUE;
 
-    public void test(ActionEvent event){
-        System.out.println("pressed");
+        double buttonCenterX = button.getLayoutX() + button.getWidth() / 2;
+        double buttonCenterY = button.getLayoutY() + button.getHeight() / 2;
+
+        for (ImageView icon : icons) {
+            double iconCenterX = icon.getLayoutX() + icon.getFitWidth() / 2;
+            double iconCenterY = icon.getLayoutY() + icon.getFitHeight() / 2;
+
+            if (iconCenterX >= button.getLayoutX() && iconCenterX <= button.getLayoutX() + button.getWidth()
+                    && iconCenterY >= button.getLayoutY() && iconCenterY <= button.getLayoutY() + button.getHeight()) {
+                double distance = Math
+                        .sqrt(Math.pow(iconCenterX - buttonCenterX, 2) + Math.pow(iconCenterY - buttonCenterY, 2));
+
+                if (distance < closestDistance) {
+                    closestIcon = icon;
+                    closestDistance = distance;
+                }
+            }
+        }
+        return closestIcon;
     }
 }
